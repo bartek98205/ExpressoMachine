bb67942422a1c04ea41e6a91a6536b45f2574e9c
Barto <bartoszhalubiec@gmail.com>
2023-11-19 20:14:14 +0100

    ExpressoMachine

--- Source/spectrumDisplay.h
+++ Source/spectrumDisplay.h
@@ -55,6 +55,80 @@ public:
 		fifo[fifoIndex++] = sample;            
 	}
 
+	void paint(juce::Graphics& g) override
+	{
+		g.fillAll(juce::Colours::black);
+
+		g.setOpacity(1.0f);
+		g.setColour(juce::Colours::white);
+		drawFrame(g);
+	}
+
+	void timerCallback() override
+	{
+		if (nextFFTBlockReady)
+		{
+			drawNextFrameOfSpectrum();
+			nextFFTBlockReady = false;
+			repaint();
+		}
+	}
+
+	void pushNextSampleIntoFifo(float sample) noexcept
+	{
+		
+		if (fifoIndex == fftSize)               
+		{
+			if (!nextFFTBlockReady)            
+			{
+				juce::zeromem(fftData, sizeof(fftData));
+				memcpy(fftData, fifo, sizeof(fifo));
+				nextFFTBlockReady = true;
+			}
+
+			fifoIndex = 0;
+		}
+
+		fifo[fifoIndex++] = sample;           
+	}
+
+	void drawNextFrameOfSpectrum()
+	{
+		
+		window.multiplyWithWindowingTable(fftData, fftSize);      
+
+	
+		fftDisp.performFrequencyOnlyForwardTransform(fftData);  
+
+		auto mindB = -100.0f;
+		auto maxdB = 0.0f;
+
+		for (int i = 0; i < scopeSize; ++i)                        
+		{
+			auto skewedProportionX = 1.0f - std::exp(std::log(1.0f - (float)i / (float)scopeSize) * 0.2f);
+			auto fftDataIndex = juce::jlimit(0, fftSize / 2, (int)(skewedProportionX * (float)fftSize * 0.5f));
+			auto level = juce::jmap(juce::jlimit(mindB, maxdB, juce::Decibels::gainToDecibels(fftData[fftDataIndex])
+				- juce::Decibels::gainToDecibels((float)fftSize)),
+				mindB, maxdB, 0.0f, 1.0f);
+
+			scopeData[i] = level;                                   
+		}
+	}
+
+	void drawFrame(juce::Graphics& g)
+	{
+		for (int i = 1; i < scopeSize; ++i)
+		{
+			auto width = getLocalBounds().getWidth();
+			auto height = getLocalBounds().getHeight();
+
+			g.drawLine({ (float)juce::jmap(i - 1, 0, scopeSize - 1, 0, width),
+								  juce::jmap(scopeData[i - 1], 0.0f, 1.0f, (float)height, 0.0f),
+						  (float)juce::jmap(i,     0, scopeSize - 1, 0, width),
+								  juce::jmap(scopeData[i],     0.0f, 1.0f, (float)height, 0.0f) });
+		}
+	}
+
 	enum
 	{
 		fftOrder = 11,
